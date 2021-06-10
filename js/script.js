adb992114f8bc51d0fda917c13d33d1747042e9b
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2021-06-10 13:24:23 +0200

    Update script.js
    Working on HTML generator scripts -> From "Wrapper" to "footer" -> outer divs ready

--- js/script.js
+++ js/script.js
@@ -2,95 +2,204 @@ fillInContent();
 
 function fillInContent() {
     const body = document.querySelector('body');
+    const mainDivNames = ["sign_log_in ", "sign_up hide", "log_in hide", "logged_in hide"];
+
     const wrapperDiv = (
         `
         <div id="wrapper">
-            ${getSignInOrLoginPage()}
-            ${getRegisterPage()}
-            ${getLoginPage()}
-            ${getLoggedInPage()}
+            ${mainDivNames.map((name) =>  
+                `
+                <div id=${name.split(" ")[0]} class=${name.split(" ")[1]}>
+                    ${generateMainDivInner(name.split(" ")[0])}
+                </div>
+                `
+            ).join("")}
         </div>
         `
     );
     body.innerHTML = wrapperDiv;
 }
+function generateMainDivInner(id) {
+    let generatedElement = "";
+    switch(id) {
+        case "sign_log_in":
+            generatedElement = generateSignInAndLogIn();
+            break;
+        case "sign_up":
+            generatedElement = generateSignUp();
+            break;
+        case "log_in":
+            generatedElement = generateLogin();
+            break;
+        case "logged_in":
+            generatedElement = generateLoggedIn();
+            break;
+    }
+    return generatedElement;
+}
+function generateSignInAndLogIn() {
+    const labelTextAndBtnValue = ["Already signed up?_Log in", "Don't have an account?_Sign up!"];
+    const divInner = (
+        `
+        <h2>My to-do application</h2>
+        <form>
+            ${labelTextAndBtnValue.map((textAndVal) => 
+                `
+                <label for="">${textAndVal.split("_")[0]}</label>
+                <input type="button" value=${textAndVal.split("_")[1]}>
+                `
+            ).join("")}
+        </form>
+        `
+    );
+    return divInner;
+}
+function generateSignUp() {
+    const idTextType = ["first_name|First name|text", "last_name|Last name|text", "email|Email|text",
+                            "password|Password|password", "aggreTerms|I agree to the Terms of Use|checkbox"];
+    const template = (
+        `
+        <h2>Sign up!</h2>
+        <form id='sign_up_form'>
+            ${idTextType.map( (specVal) => 
+                `
+                <label for=${specVal.split("|")[0]}>
+                    ${specVal.split("|")[1]}
+                </label>
+                <input id=${specVal.split("|")[0]} type=${specVal.split("|")[2]}>
+                `
+            ).join("")}
+            <input id="register_btn" type="button" value="Register">
+        </form>
+        `
+    );
+    return template;
+}
+function generateLogin() {
+    const idTextType = ["email_log|Email|text", "psw_log|Password|password", ""];
 
-function getSignInOrLoginPage() {
-    const buttonValues = ["Log In", "Sign up!"];
-    const signInOrLoginDiv = (
+    const template = (
         `
-        <div id="sign_log_in">
-            <h2>My to-do application</h2>
-            <form>
-                <label for="">Already signed up?</label>
-                <input type="button" value="Log In">
-                <label for="">Don't have an account?</label>
-                <input type="button" value="Sign up!">
-            </form>
-        </div>
-        `
-    );
-    return signInOrLoginDiv;
-}
-function getRegisterPage() {
-    const ids = ["first_name", "last_name", "email", "password", "aggreTerms", "register_btn"];
-    const types = ["text", "password", "checkbox", "button"];
-    const registerDiv = (
-        `
-        <div id="sign_up" class="hide">
-            <h2>Sign up!</h2>
-            <form id='sign_up_form'>
-                
-                <label for="first_name">First name</label>
-                <input id='first_name' type="text">
-                <label for="last_name">Last name</label>
-                <input id='last_name' type="text">
-                <label for="email">Email</label>
-                <input id='email' type="text">
-                <label for="password">Password</label>
-                <input id='password' type="password">
-                <label for="">I agree to the Terms of Use</label>
-                <input id="aggreTerms" type="checkbox">
-                <input id="register_btn" type="button" value="Register">
-            </form>
-        </div>
+        <h2>Login</h2>
+        <form id="login_form">
+            ${idTextType.map((specVal) => 
+                `
+                <label for=${specVal.split("|")[0]}>
+                    ${specVal.split("|")[1]}
+                </Label>
+                <input id=${specVal.split("|")[0]} type=${specVal.split("|")[2]}>
+                `
+            ).join("")}
+            <input id="login_btn" type="button" value="Login"> 
+        </form>
         `
     );
-    return registerDiv;
+
+    return template;
 }
-function getLoginPage(){
-    const loginDiv = (
+function generateLoggedIn() {
+    const classNames =["top_menu","main_content","footer"];
+    const template = (
         `
-        <div id="log_in" class="hide">
-            <h2>Login</h2>
-            <form id="login_form">
-                <label for="email_log">Email</label>
-                <input id="email_log" type="text">
-                <label for="psw_log">Password</label>
-                <input id="psw_log" type="password">
-                <input id="login_btn" type="button" value="Login"> 
-            </form>
-        </div>
-        `
-    );
-    return loginDiv;
-}
-function getLoggedInPage() {
-    const buttonValues = ["My list", "Settings", "Log out"];
-    const getLoggedInPage = ( 
-        `
-        <div id="logged_in" class="hide">
-            <div class="top_menu">
-                <h2>Welcome <span id='welcome_name'></span><span id='welcome_email' class='hide'></span></h2>
-                <form>
-                    ${buttonValues.map((value) => `<input type="button" value=${value}></input>`)}
-                </form>
+        ${classNames.map((name) => 
+            `
+            <div class=${name}>
+                ${generateLoggedInInnerDivs(name)}
             </div>
-        </div>
+            `
+        ).join("")}
         `
-        );
-    return getLoggedInPage;
-};
+    );
+    return template;
+}
+function generateLoggedInInnerDivs(className) {
+    let template;
+
+    switch(className) {
+        case "top_menu":
+            template = generateTopMenu();
+            break;
+        case "main_content":
+            template = generateMainContent();
+            break;
+        case "footer":
+            template = generateFooter();
+            break;
+        
+    }
+    return template;
+}
+function generateTopMenu() {
+    const spanIds = ["welcome_name|", "welcome_email|hide"];
+    const template =(
+        `
+        <h2>Welcome  
+            ${spanIds.map((id) => 
+                `<span id=${id.split("|")[0]} class=${id.split("|")[1]}></span>`
+            ).join("")}
+        </h2>
+        `
+    );
+    return template;
+}
+function generateMainContent() {
+    const ids = ["my_list_div|", "account_settings|hide"];
+    const template = (
+        `
+        ${ids.map((specId) =>
+            `
+            <div id=${specId.split("|")[0]} class=${specId.split("|")[1]}>
+                ${generateMainContentInnerDivs(specId.split("|")[0])}
+            </div>
+            `
+        ).join("")}
+                        `
+    );
+    return template;
+}
+function generateMainContentInnerDivs(outerClassName) {
+    let template;
+
+    switch(outerClassName) {
+        case "my_list_div":
+            template = generateMyListDiv();
+            break;
+        case "account_settings":
+            break;
+    }
+    return template;
+}
+function generateMyListDiv() {
+    const specIds = ["my_list_div_menu|", "existing_lists|my_list_box", "create_new_list|hide"];
+    const template = (
+        `
+        ${specIds.map((specId) => 
+            `
+            <div id=${specId.split("|")[0]} class=${specId.split("|")[1]}>
+                ${generateMyListDivsInner(specId.split("|")[0])}
+            </div>
+            `
+        ).join("")}
+        `
+    );
+    return template;
+}
+function generateMyListDivsInner(id) {
+    let result;
+
+    switch(id) {
+        case "my_list_div_menu":
+            break;
+        case "existing_lists":
+            break;
+        case "create_new_list":
+            break;
+    }
+    return result;
+}
+function generateFooter() {
+    return null;
+}
 /*
     Existing lists and create new buttons handlers
 */
@@ -138,10 +247,10 @@ function include(file) {
     document.getElementsByTagName('body').item(0).appendChild(script);
 }
 
-include('js/loginHandler.js');
-include('js/loginAndSignupHandler.js');
+//include('js/loginHandler.js');
+//include('js/loginAndSignupHandler.js');
 //include('js/userRegistration.js');
-include('js/userLoginHandler.js');
+//include('js/userLoginHandler.js');
 //include('js/existingListsHandler.js');
 //include('js/userSettings.js');
 //include('js/createToDoList.js');
